4fa98896fc3e639a8ebabf350c2356de1e5e51d4
rohan <rohanpatil362@gmail.com>
2026-03-28 19:12:21 +0530

    Updated Login Test

--- src/test/java/com/ui/tests/LoginTest.java
+++ src/test/java/com/ui/tests/LoginTest.java
@@ -20,11 +20,11 @@ public class LoginTest extends BaseTest{
 		Logger logger = LoggerUtility.getLogger(this.getClass());
 		
 	
-//		@Test(description = "Verify login using with valid login credentials", groups= {"e2e", "sanity"}, 
-//				dataProviderClass=com.ui.dataproviders.LoginDataProviders.class, dataProvider="LoginTestProvider")
-//		public void verifyLoginTest(User user) {
-//		assertEquals(landingPage.goToLoginPage().peformLogin(user.getEmail(), user.getPassword()).getUserName(), "Rohan Patil");
-//	}
+		@Test(description = "Verify login using with valid login credentials", groups= {"e2e", "sanity"}, 
+				dataProviderClass=com.ui.dataproviders.LoginDataProviders.class, dataProvider="LoginTestProvider")
+		public void verifyLoginTest(User user) {
+		assertEquals(landingPage.goToLoginPage().peformLogin(user.getEmail(), user.getPassword()).getUserName(), "Rohan Patil");
+	}
 		
 		
 //		@Test(description = "Verify login using with valid login credentials", groups= {"e2e", "sanity"}, 
@@ -33,14 +33,14 @@ public class LoginTest extends BaseTest{
 //		assertEquals(landingPage.goToLoginPage().peformLogin(user.getEmail(), user.getPassword()).getUserName(), "Rohan Patil");
 //	}
 		
-		@Test(description = "Verify login using with valid login credentials", groups= {"e2e", "sanity"}, 
-				dataProviderClass=com.ui.dataproviders.LoginDataProviders.class, dataProvider="LoginTestExcelDataProvider",
-				retryAnalyzer=com.ui.listeners.MyRetryAnalyzer.class)
-		public void verifyoginTestExcel(User user) {
-			logger.info("Started my login excel test");
-		assertEquals(landingPage.goToLoginPage().peformLogin(user.getEmail(), user.getPassword()).getUserName(), "Rohan Patil");
-		logger.info("login excel test completed");
-	}
+//		@Test(description = "Verify login using with valid login credentials", groups= {"e2e", "sanity"}, 
+//				dataProviderClass=com.ui.dataproviders.LoginDataProviders.class, dataProvider="LoginTestExcelDataProvider",
+//				retryAnalyzer=com.ui.listeners.MyRetryAnalyzer.class)
+//		public void verifyoginTestExcel(User user) {
+//			logger.info("Started my login excel test");
+//		assertEquals(landingPage.goToLoginPage().peformLogin(user.getEmail(), user.getPassword()).getUserName(), "Rohan Patil");
+//		logger.info("login excel test completed");
+//	}
 		
 		
 
